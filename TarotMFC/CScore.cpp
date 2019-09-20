b316b4fda1a20f0c2e3cf03ee1ff0d34dae6c29d
saintpaul <vivien.saint-paul@hotmail.fr>
2019-09-20 14:24:24 +0200

    scores 0.1

--- TarotMFC/CScore.cpp
+++ TarotMFC/CScore.cpp
@@ -5,6 +5,8 @@
 #include "TarotMFC.h"
 #include "CScore.h"
 #include "afxdialogex.h"
+#include "CPartie.h"
+
 
 
 // Boîte de dialogue CScore
@@ -12,7 +14,15 @@
 IMPLEMENT_DYNAMIC(CScore, CDialogEx)
 
 CScore::CScore(CWnd* pParent /*=NULL*/)
-	: CDialogEx(IDD_TAROTMFC_SCORES, pParent)
+	: CDialogEx(IDD_TARROTMFC_SCORE, pParent)
+	, nomJoueur1(_T(""))
+	, nomJoueur2(_T(""))
+	, nomJoueur3(_T(""))
+	, nomJoueur4(_T(""))
+	, score1(_T(""))
+	, score2(_T(""))
+	, score3(_T(""))
+	, score4(_T(""))
 {
 
 }
@@ -24,59 +34,24 @@ CScore::~CScore()
 void CScore::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, ID_NOM_1, nomJoueur1);
+	DDX_Text(pDX, ID_NOM_2, nomJoueur2);
+	DDX_Text(pDX, ID_NOM_3, nomJoueur3);
+	DDX_Text(pDX, ID_NOM_4, nomJoueur4);
+	DDX_Text(pDX, ID_SCORE_1, score1);
+	DDX_Text(pDX, ID_SCORE_2, score2);
+	DDX_Text(pDX, ID_SCORE_3, score3);
+	DDX_Text(pDX, ID_SCORE_4, score4);
+}
+
+void CScore::afficherNom(CJoueur lesJoueurs[])
+{
+	nomJoueur1 = lesJoueurs[0];
 }
 
 
 BEGIN_MESSAGE_MAP(CScore, CDialogEx)
-	ON_EN_CHANGE(IDC_EDIT1, &CScore::OnEnChangeEdit1)
-	ON_EN_CHANGE(IDC_EDIT3, &CScore::OnEnChangeEdit3)
-	ON_EN_CHANGE(IDC_EDIT2, &CScore::OnEnChangeEdit2)
-	ON_EN_CHANGE(IDC_EDIT4, &CScore::OnEnChangeEdit4)
 END_MESSAGE_MAP()
 
 
 // Gestionnaires de messages de CScore
-
-
-void CScore::OnEnChangeEdit1()
-{
-	// TODO:  S'il s'agit d'un contrôle RICHEDIT, le contrôle ne
-	// envoyez cette notification sauf si vous substituez CDialogEx::OnInitDialog()
-	// fonction et appelle CRichEditCtrl().SetEventMask()
-	// avec l'indicateur ENM_CHANGE ajouté au masque grâce à l'opérateur OR.
-
-	// TODO:  Ajoutez ici le code de votre gestionnaire de notification de contrôle
-}
-
-
-void CScore::OnEnChangeEdit3()
-{
-	// TODO:  S'il s'agit d'un contrôle RICHEDIT, le contrôle ne
-	// envoyez cette notification sauf si vous substituez CDialogEx::OnInitDialog()
-	// fonction et appelle CRichEditCtrl().SetEventMask()
-	// avec l'indicateur ENM_CHANGE ajouté au masque grâce à l'opérateur OR.
-
-	// TODO:  Ajoutez ici le code de votre gestionnaire de notification de contrôle
-}
-
-
-void CScore::OnEnChangeEdit2()
-{
-	// TODO:  S'il s'agit d'un contrôle RICHEDIT, le contrôle ne
-	// envoyez cette notification sauf si vous substituez CDialogEx::OnInitDialog()
-	// fonction et appelle CRichEditCtrl().SetEventMask()
-	// avec l'indicateur ENM_CHANGE ajouté au masque grâce à l'opérateur OR.
-
-	// TODO:  Ajoutez ici le code de votre gestionnaire de notification de contrôle
-}
-
-
-void CScore::OnEnChangeEdit4()
-{
-	// TODO:  S'il s'agit d'un contrôle RICHEDIT, le contrôle ne
-	// envoyez cette notification sauf si vous substituez CDialogEx::OnInitDialog()
-	// fonction et appelle CRichEditCtrl().SetEventMask()
-	// avec l'indicateur ENM_CHANGE ajouté au masque grâce à l'opérateur OR.
-
-	// TODO:  Ajoutez ici le code de votre gestionnaire de notification de contrôle
-}
--- TarotMFC/CScore.h
+++ TarotMFC/CScore.h
@@ -13,16 +13,22 @@ public:
 
 // Données de boîte de dialogue
 #ifdef AFX_DESIGN_TIME
-	enum { IDD = IDD_TAROTMFC_SCORES };
+	enum { IDD = IDD_TARROTMFC_SCORE };
 #endif
 
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // Prise en charge de DDX/DDV
 
 	DECLARE_MESSAGE_MAP()
+private:
+	CString nomJoueur1;
+	CString nomJoueur2;
+	CString nomJoueur3;
+	CString nomJoueur4;
+	CString score1;
+	CString score2;
+	CString score3;
+	CString score4;
 public:
-	afx_msg void OnEnChangeEdit1();
-	afx_msg void OnEnChangeEdit3();
-	afx_msg void OnEnChangeEdit2();
-	afx_msg void OnEnChangeEdit4();
+	void afficherNom(CJoueur lesJoueurs[]);
 };
